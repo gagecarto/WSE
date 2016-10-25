0362ad056ad2990e916b5ffc0af1fe0fe6b3c4bc
gagecarto <josh@gagecarto.com>
2016-10-25 10:24:19 -0600

    updating mapbox credens

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 
 var numberOfFiles=0;
 
-function initialize() {
-	L.mapbox.accessToken = 'pk.eyJ1Ijoic25vd2dhZ2UiLCJhIjoiUnhjQVVqMCJ9.IwvvLu1WhgtaSryHAf5Daw';
+function initialize() {	
+	L.mapbox.accessToken = 'pk.eyJ1Ijoid2VzdGVybnN1c3RhaW5hYmlsaXR5IiwiYSI6ImNpdXBwOTZrbDAyYzQyeXJ0c2xwOTF3azEifQ.lTej0CWbl4nR4N17KTf-sQ'
 	var map = L.mapbox.map('map', 'mapbox.streets');
 
 	map.setView([45.2,-111],8);
@@ -40,119 +40,119 @@ function initialize() {
 	//publicLands
 	cartodb.createLayer(map, pubLandsViz,{
 		legends:false,
-		loaderControl:true,      
+		loaderControl:true,
 		})
 		.addTo(map)
-		.on('done', function(layer) {      
-		  pubLandsLayer=layer;  
+		.on('done', function(layer) {
+		  pubLandsLayer=layer;
 		  mapLayers.pubLands=pubLandsLayer;
 		  mapLayers.pubLands.type='cartoDBLayer';
 		  mapLayers.pubLands.hide();
-		  layer.hide(); 		  
-		  pubLandsLayer		    		    
+		  layer.hide();
+		  pubLandsLayer
 		    .on('featureOver',function(e,latlng, pos, data){
-		      $("#map").css( 'cursor', 'pointer' );                    
-		    })     
-		    .on('featureOut',function(e,latlng, pos, data){ 
+		      $("#map").css( 'cursor', 'pointer' );
+		    })
+		    .on('featureOut',function(e,latlng, pos, data){
 		      $("#map").css( 'cursor', '-moz-grab' );
-		      $("#map").css( 'cursor', '-webkit-grab' );          
-		    })     
+		      $("#map").css( 'cursor', '-webkit-grab' );
+		    })
 		})
 
 	//grizzly
 	cartodb.createLayer(map, grizViz,{
 		legends:false,
-		loaderControl:true,      
+		loaderControl:true,
 		})
 		.addTo(map)
-		.on('done', function(layer) {      
-		  grizLayer=layer;  
+		.on('done', function(layer) {
+		  grizLayer=layer;
 		  mapLayers.grizLayer=grizLayer;
 		  mapLayers.grizLayer.type='cartoDBLayer';
-		  mapLayers.grizLayer.hide();		  	  
+		  mapLayers.grizLayer.hide();
 		})
 
 	//elk
 	cartodb.createLayer(map, elkViz,{
 		legends:false,
-		loaderControl:true,      
+		loaderControl:true,
 		})
 		.addTo(map)
-		.on('done', function(layer) {      
-		  elkLayer=layer;  
+		.on('done', function(layer) {
+		  elkLayer=layer;
 		  mapLayers.elkLayer=elkLayer;
 		  mapLayers.elkLayer.type='cartoDBLayer';
-		  mapLayers.elkLayer.hide();		  		  		  
+		  mapLayers.elkLayer.hide();
 		})
 
 	//HUC 250
 	cartodb.createLayer(map, hydroViz,{
 		legends:false,
-		loaderControl:true,      
+		loaderControl:true,
 		})
 		.addTo(map)
-		.on('done', function(layer) {      
-		  hydroLayer=layer;  
+		.on('done', function(layer) {
+		  hydroLayer=layer;
 		  mapLayers.hydroLayer=hydroLayer;
 		  mapLayers.hydroLayer.type='cartoDBLayer';
-		  mapLayers.hydroLayer.hide();				  
+		  mapLayers.hydroLayer.hide();
 		})
 
 	//sage grouse
 	cartodb.createLayer(map, sageGrouseViz,{
 		legends:false,
-		loaderControl:true,      
+		loaderControl:true,
 		})
 		.addTo(map)
-		.on('done', function(layer) {      
-		  sageGrouseLayer=layer;  
+		.on('done', function(layer) {
+		  sageGrouseLayer=layer;
 		  mapLayers.sageGrouseLayer=sageGrouseLayer;
 		  mapLayers.sageGrouseLayer.type='cartoDBLayer';
-		  mapLayers.sageGrouseLayer.hide();				  
+		  mapLayers.sageGrouseLayer.hide();
 		})
 
 	//wolves
 	cartodb.createLayer(map, wolvesViz,{
 		legends:false,
-		loaderControl:true,      
+		loaderControl:true,
 		})
 		.addTo(map)
-		.on('done', function(layer) {      
-		  wolvesLayer=layer;  
+		.on('done', function(layer) {
+		  wolvesLayer=layer;
 		  mapLayers.wolvesLayer=wolvesLayer;
 		  mapLayers.wolvesLayer.type='cartoDBLayer';
-		  mapLayers.wolvesLayer.hide();				  
+		  mapLayers.wolvesLayer.hide();
 		})
 	//conservation value
 	cartodb.createLayer(map, consValueViz,{
 		legends:false,
-		loaderControl:true,      
+		loaderControl:true,
 		})
 		.addTo(map)
-		.on('done', function(layer) {      
-		  consValueLayer=layer;  
+		.on('done', function(layer) {
+		  consValueLayer=layer;
 		  mapLayers.consValueLayer=consValueLayer;
 		  mapLayers.consValueLayer.type='cartoDBLayer';
-		  mapLayers.consValueLayer.hide();				  
+		  mapLayers.consValueLayer.hide();
 		})
 
 	//important bird areas
 	cartodb.createLayer(map, ibaViz,{
 		legends:false,
-		loaderControl:true,      
+		loaderControl:true,
 		})
 		.addTo(map)
-		.on('done', function(layer) {      
-		  ibaLayer=layer;  
+		.on('done', function(layer) {
+		  ibaLayer=layer;
 		  mapLayers.ibaLayer=ibaLayer;
 		  mapLayers.ibaLayer.type='cartoDBLayer';
-		  mapLayers.ibaLayer.hide();				  
+		  mapLayers.ibaLayer.hide();
 		})
 
 
 
-	function buildDropDowns() {		
-		$.each(dataStructure, function(i, item) {						
+	function buildDropDowns() {
+		$.each(dataStructure, function(i, item) {
 		    var thisItem=item;
 			$('#sidebar').append("<div class='menuSubtitle'>" + thisItem.name + "</div><select id='" + thisItem.id + "' class='multiselectClass' multiple='multiple'></select><br>")
 			window[thisItem.id + "Layers"] = [];
@@ -192,9 +192,9 @@ function initialize() {
 					})
 				}, 10);
 			})
-		})	
+		})
 	}
-	buildDropDowns();	
+	buildDropDowns();
 
 	var markerColors=['#D64783','#40202A','#FD135A','#84C1B1'];
 
@@ -202,7 +202,7 @@ function initialize() {
 
 	function addMapLayers(){
 		$.each(dataStructure, function(i, item) {
-			var thisItem = item;			
+			var thisItem = item;
 			if(thisItem.type=='kmlLayers'){
 				//create a style for this group
 				window[thisItem.id+'Style']=L.mapbox.marker.icon({
@@ -210,50 +210,50 @@ function initialize() {
 			        'stroke-width': 0.1,
 			        'marker-symbol': 'marker-stroked',
 			        'marker-color': markerColors[colorNumber],
-			    })		
-			    colorNumber+=1;	
-				var thisGroupId=item.id;					
+			    })
+			    colorNumber+=1;
+				var thisGroupId=item.id;
 				for (var j = 0; j < thisItem.files.length; j++) {
-					var thisFile = thisItem.files[j];	
-					var thisFileId= thisFile.id;							
-					//window[thisFileId] = omnivore.kml(thisItem.path+thisFile.fileName)			    
+					var thisFile = thisItem.files[j];
+					var thisFileId= thisFile.id;
+					//window[thisFileId] = omnivore.kml(thisItem.path+thisFile.fileName)
 					mapLayers[thisFile.id] = omnivore.kml(thisItem.path+thisFile.fileName);
 					mapLayers[thisFile.id].group=thisGroupId;
 					mapLayers[thisFile.id].type='kmlLayer';
-				}		
+				}
 			}
-		})		
+		})
 	}
-	addMapLayers();	
+	addMapLayers();
 
 	function assignLayerSymbologies(){
-		$.each(mapLayers, function(i, item) {				
+		$.each(mapLayers, function(i, item) {
 			if(item.type=='kmlLayer'){
 				var thisGroup=item.group;
-				mapLayers[i].eachLayer(function(layer) {				
-		        	var thisName=layer.feature.properties.name;	        	
-		        	layer.bindPopup(thisName);	        		        	
-		        	layer.setIcon(eval(thisGroup+'Style')); 	        	
+				mapLayers[i].eachLayer(function(layer) {
+		        	var thisName=layer.feature.properties.name;
+		        	layer.bindPopup(thisName);
+		        	layer.setIcon(eval(thisGroup+'Style'));
 		        });
 			}
 		});
 	}
 
 	var first=true;
-	function layerShower(layerToShow) {		
+	function layerShower(layerToShow) {
 		if(first){
 			assignLayerSymbologies();
 			first=false;
-		}		
+		}
 		if(mapLayers[layerToShow].type=='cartoDBLayer'){
 			mapLayers[layerToShow].show();
 		}
 		if(mapLayers[layerToShow].type=='kmlLayer'){
-			map.addLayer(mapLayers[layerToShow]);			
-		}		
+			map.addLayer(mapLayers[layerToShow]);
+		}
 	}
 
-	function layerHider(layerToHide) {							
+	function layerHider(layerToHide) {
 		if(mapLayers[layerToHide].type=='cartoDBLayer'){
 			mapLayers[layerToHide].hide();
 		}
